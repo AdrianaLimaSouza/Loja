2a86262aac45b220ddd90cc55bc1d19547058368
AdrianaLimaSouza <didalyma@hotmail.com>
2026-02-24 20:55:25 +0000

    Initial commit

---
+++ Loja/Form1.Designer.vb
@@ -0,0 +1,191 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtNome = New System.Windows.Forms.TextBox()
+        Me.txtPreco = New System.Windows.Forms.TextBox()
+        Me.picFoto = New System.Windows.Forms.PictureBox()
+        Me.btnInserir = New System.Windows.Forms.Button()
+        Me.btnActualizar = New System.Windows.Forms.Button()
+        Me.btnEliminar = New System.Windows.Forms.Button()
+        Me.btnImagem = New System.Windows.Forms.Button()
+        Me.dgvProdutos = New System.Windows.Forms.DataGridView()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picFoto, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.dgvProdutos, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Loja.My.Resources.Resources.BannerLoja
+        Me.PictureBox1.Location = New System.Drawing.Point(-4, -3)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(805, 136)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 147)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(93, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Nome do Produto:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 186)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(38, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Preço:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(332, 147)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(86, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Foto do Produto:"
+        '
+        'txtNome
+        '
+        Me.txtNome.Location = New System.Drawing.Point(123, 147)
+        Me.txtNome.Name = "txtNome"
+        Me.txtNome.Size = New System.Drawing.Size(100, 20)
+        Me.txtNome.TabIndex = 4
+        '
+        'txtPreco
+        '
+        Me.txtPreco.Location = New System.Drawing.Point(123, 183)
+        Me.txtPreco.Name = "txtPreco"
+        Me.txtPreco.Size = New System.Drawing.Size(100, 20)
+        Me.txtPreco.TabIndex = 5
+        '
+        'picFoto
+        '
+        Me.picFoto.Location = New System.Drawing.Point(442, 147)
+        Me.picFoto.Name = "picFoto"
+        Me.picFoto.Size = New System.Drawing.Size(333, 140)
+        Me.picFoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.picFoto.TabIndex = 6
+        Me.picFoto.TabStop = False
+        '
+        'btnInserir
+        '
+        Me.btnInserir.Location = New System.Drawing.Point(15, 237)
+        Me.btnInserir.Name = "btnInserir"
+        Me.btnInserir.Size = New System.Drawing.Size(75, 23)
+        Me.btnInserir.TabIndex = 7
+        Me.btnInserir.Text = "Adicionar"
+        Me.btnInserir.UseVisualStyleBackColor = True
+        '
+        'btnActualizar
+        '
+        Me.btnActualizar.Location = New System.Drawing.Point(123, 237)
+        Me.btnActualizar.Name = "btnActualizar"
+        Me.btnActualizar.Size = New System.Drawing.Size(75, 23)
+        Me.btnActualizar.TabIndex = 8
+        Me.btnActualizar.Text = "Actualizar"
+        Me.btnActualizar.UseVisualStyleBackColor = True
+        '
+        'btnEliminar
+        '
+        Me.btnEliminar.Location = New System.Drawing.Point(242, 237)
+        Me.btnEliminar.Name = "btnEliminar"
+        Me.btnEliminar.Size = New System.Drawing.Size(75, 23)
+        Me.btnEliminar.TabIndex = 9
+        Me.btnEliminar.Text = "Eliminar"
+        Me.btnEliminar.UseVisualStyleBackColor = True
+        '
+        'btnImagem
+        '
+        Me.btnImagem.Location = New System.Drawing.Point(335, 186)
+        Me.btnImagem.Name = "btnImagem"
+        Me.btnImagem.Size = New System.Drawing.Size(75, 42)
+        Me.btnImagem.TabIndex = 10
+        Me.btnImagem.Text = "Imagem"
+        Me.btnImagem.UseVisualStyleBackColor = True
+        '
+        'dgvProdutos
+        '
+        Me.dgvProdutos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvProdutos.Location = New System.Drawing.Point(12, 303)
+        Me.dgvProdutos.Name = "dgvProdutos"
+        Me.dgvProdutos.Size = New System.Drawing.Size(763, 205)
+        Me.dgvProdutos.TabIndex = 11
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(803, 520)
+        Me.Controls.Add(Me.dgvProdutos)
+        Me.Controls.Add(Me.btnImagem)
+        Me.Controls.Add(Me.btnEliminar)
+        Me.Controls.Add(Me.btnActualizar)
+        Me.Controls.Add(Me.btnInserir)
+        Me.Controls.Add(Me.picFoto)
+        Me.Controls.Add(Me.txtPreco)
+        Me.Controls.Add(Me.txtNome)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picFoto, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dgvProdutos, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtNome As TextBox
+    Friend WithEvents txtPreco As TextBox
+    Friend WithEvents picFoto As PictureBox
+    Friend WithEvents btnInserir As Button
+    Friend WithEvents btnActualizar As Button
+    Friend WithEvents btnEliminar As Button
+    Friend WithEvents btnImagem As Button
+    Friend WithEvents dgvProdutos As DataGridView
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+End Class
---
+++ Loja/Form1.vb
@@ -0,0 +1,119 @@
+﻿
+Imports System.Data.SqlClient 'Namespace quando trabalhamos com DB SQL
+Imports System.IO 'quando utilizamos imagens num projeto com base de Dados temos que usar este namespace (Imports.IO)
+' esta é uma Aplicação CRUD (Criar, Ler, Atualizar e Deletar) 
+Public Class Form1
+
+    Dim con As SqlConnection = New SqlConnection("Data Source=(localdb)\MSSQLLocalDB;Database=Loja;Integrated Security=true;") 'Isto é a ligação a Base de Dados e sempre tem que ser colocada
+    Dim ImagemBytes() As Byte 'variável para Imagens
+
+    Sub CarregarDados()
+        Dim da As New SqlDataAdapter("SELECT * FROM Produtos", con) ' criamos a variável
+        Dim dt As New DataTable
+
+        da.Fill(dt) ' a variável da irá preencher o datagridview e criar a tabela
+
+        dgvProdutos.DataSource = dt
+
+        'Formatar com Currency na Coluna Preço
+        dgvProdutos.Columns("Preco").DefaultCellStyle.Format = "C2"
+        dgvProdutos.Columns("Preco").DefaultCellStyle.FormatProvider = Globalization.CultureInfo.GetCultureInfo("pt-PT")
+
+        Dim pic1 As New DataGridViewImageColumn
+        pic1 = dgvProdutos.Columns(3)
+        dgvProdutos.RowTemplate.Height = 70
+
+
+        pic1.ImageLayout = DataGridViewImageCellLayout.Zoom
+
+        dgvProdutos.AllowUserToAddRows = False
+
+    End Sub
+
+    Private Sub btnInserir_Click(sender As Object, e As EventArgs) Handles btnInserir.Click
+        Dim Preco As Decimal
+        Decimal.TryParse(txtPreco.Text, Preco)
+
+        Dim cmd As New SqlCommand("INSERT into Produtos(NomeProduto,Preco,Foto) VALUES (@n,@p,@f)", con)
+
+        cmd.Parameters.AddWithValue("@n", txtNome.Text)
+        cmd.Parameters.AddWithValue("@p", Preco)
+        cmd.Parameters.AddWithValue("@f", ImagemBytes)
+
+        con.Open()
+        cmd.ExecuteNonQuery()
+        MessageBox.Show("Registo do Produto submetido com sucesso")
+
+        con.Close()
+
+        CarregarDados()
+    End Sub
+
+    Private Sub btnImagem_Click(sender As Object, e As EventArgs) Handles btnImagem.Click
+        Dim ofd As New OpenFileDialog
+        ofd.Filter = "fotosLoja|*.jpg;*.png"
+
+        If ofd.ShowDialog = DialogResult.OK Then
+            picFoto.Image = Image.FromFile(ofd.FileName)
+            ImagemBytes = File.ReadAllBytes(ofd.FileName)
+        End If
+
+    End Sub
+
+    Private Sub btnActualizar_Click(sender As Object, e As EventArgs) Handles btnActualizar.Click
+        Dim id As Integer = dgvProdutos.CurrentRow.Cells("Id").Value
+
+        ' Dim Preco As Decimal
+        'Decimal.TryParse(txtPreco.Text, Preco)
+        Dim Preco As Decimal = CDec(txtPreco.Text)
+
+        Dim cmd As New SqlCommand("UPDATE Produtos SET NomeProduto=@n,Preco=@p,Foto=@f WHERE id=" & id, con)
+
+        cmd.Parameters.AddWithValue("@n", txtNome.Text)
+        cmd.Parameters.AddWithValue("@p", Preco)
+        cmd.Parameters.AddWithValue("@f", ImagemBytes)
+        cmd.Parameters.AddWithValue("@id", id)
+
+        con.Open()
+        cmd.ExecuteNonQuery()
+        MessageBox.Show("Produto actualizado com sucesso")
+
+        con.Close()
+
+        CarregarDados()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        CarregarDados()
+    End Sub
+
+    Private Sub dgvProdutos_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvProdutos.CellClick
+
+        txtNome.Text = dgvProdutos.CurrentRow.Cells("NomeProduto").Value
+
+        Dim Valor As Decimal = dgvProdutos.CurrentRow.Cells("Preco").Value
+        dgvProdutos.Columns("Preco").DefaultCellStyle.Format = "C2"
+        txtPreco.Text = Valor.ToString("C2", Globalization.CultureInfo.GetCultureInfo("pt-PT"))
+
+        'Mostrar imagem na PictureBox
+
+        ImagemBytes = dgvProdutos.CurrentRow.Cells("Foto").Value
+        Dim ms As New MemoryStream(ImagemBytes)
+        picFoto.Image = Image.FromStream(ms)
+
+    End Sub
+
+    Private Sub btnEliminar_Click(sender As Object, e As EventArgs) Handles btnEliminar.Click
+        Dim id As Integer = dgvProdutos.CurrentRow.Cells("Id").Value
+
+        Dim cmd As New SqlCommand("DELETE FROM Produtos WHERE id=" & id, con)
+
+        con.Open()
+        cmd.ExecuteNonQuery()
+        MessageBox.Show("Produto eliminado com sucesso")
+
+        con.Close()
+
+        CarregarDados()
+    End Sub
+End Class
